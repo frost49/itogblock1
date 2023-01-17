8518122841e61310f1b764f4300ea8f1cf96e69a
frost49 <frost49@mail.ru>
2023-01-17 19:44:20 +0400

    Первая проба

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Vfccbd rfrjq lkbyys? ");
+int size  = int.Parse(Console.ReadLine()!);
+
+string[] arr1 = new string[size];
+
+for (int i = 0; i < size; i++){
+	Console.Write("'ktvtyn vfccbdf: ");
+    string result = Console.ReadLine();
+	arr1[i] = result;  
+}
+Console.WriteLine();
+Console.Write("Введенный массив: [");
+Console.Write(string.Join(", ", arr1));
+Console.Write("]");
+
+int count = 0;
+int maxSymbols = 3;
+
+for (int i = 0; i < arr1.Length; i++){
+	if(arr1[i].Length <= maxSymbols){
+		count++;
+	}   
+}
+Console.WriteLine();
+Console.Write("Полученный массив из строк, длина которых меньше либо равна трём символам: [");
+string[] arr2 = new string[count];
+int j = 0;
+for (int i = 0; i < arr1.Length; i++){
+    if(arr1[i].Length <= maxSymbols){
+        arr2[j] = arr1[i];
+        Console.Write(arr2[j] + " ");
+        j++;
+    }
+}
+Console.Write("]");
